02c487bf5cc274c7bea89fa5560d41de195c7284
matejsarlija <matejsarlija1@gmail.com>
2023-07-25 22:28:54 +0200

    figuring out what the H is fit() exactly

--- perceptron.py
+++ perceptron.py
@@ -1,2 +1,44 @@
+import numpy as np
+
+
+"""
+Perceptron... classifier I guess?
+"""
+
+
+"""
+params:
+eta : learning rate
+n_iter : epochs
+random_state: random generator seed
+"""
 class Perceptron:
-    pass
+    def __init__(self, eta=0.01, n_iter=50, random_state=1) -> None:
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """
+        X - shape : [n_examples, n_features]
+        y - shape : [n_examples]
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01,
+                              size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for x_i, target in zip(X, y):
+                update = self.eta * (target - self.predict(x_i))
+                self.w_ += update * x_i
+                self.b_ += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        pass
+
